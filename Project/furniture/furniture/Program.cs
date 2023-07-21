755d74eb48c2c9f70fb0158a6b3e622293f9a417
Priyadarshini <pr7159@srmist.edu.in>
2023-07-21 10:56:16 +0530

    Add files via upload

---
+++ Project/furniture/furniture/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Concept.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            Chair chair = new Chair();
+
+        }
+    }
+}
