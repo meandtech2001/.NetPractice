fd751e08d63d240b6ede421416eb6591301749b9
Priyadarshini <pr7159@srmist.edu.in>
2023-07-21 10:47:10 +0530

    Add files via upload

---
+++ Project/BankAccount/app1Test/App1Tests.cs
@@ -0,0 +1,57 @@
+﻿using App01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Principal;
+
+namespace App1Tests
+{
+    [TestClass]
+    public class App1Tests
+    {
+        string name = "priya sharma";
+        int accountNumber = 1;
+        double balance = 20000;
+        string password = "pass123";
+        double interestRate = 9.5;
+        BankAccount account;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            account = new BankAccount(accountNumber, name, password, balance, interestRate);
+        }
+
+        [TestMethod]
+        
+        public void Withdraw_ShouldFailForNegativeAmount()
+        {
+
+            var result = account.Withdraw(-1, password);
+            Assert.AreEqual(result, "Invalid Amount");
+        }
+        [TestMethod]
+        public void Withdraw_ShouldFailForInvalidCredentials()
+        {
+
+            var result = account.Withdraw(1, "pass12");
+            Assert.AreEqual(result, "Invalid Credentials");
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldFailForInsufficientBalance()
+        {
+
+            var result = account.Withdraw(100000, password);
+            Assert.AreEqual(result, "Insufficient Balance");
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldSucceedInCorrectPath()
+        {
+
+            var result = account.Withdraw(100, password);
+            //Assert.IsTrue(result);
+            Assert.AreEqual(result, "Success");
+        }
+    }
+}
---
+++ Project/BankAccount/app1Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("app1Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("app1Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c88006f7-b2c3-4d3c-8d4e-d6bc39a5df99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
