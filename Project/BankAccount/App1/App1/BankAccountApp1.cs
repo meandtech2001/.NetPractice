223083232731fb8c062ec2daa8534e56d1b50969
Priyadarshini <pr7159@srmist.edu.in>
2023-07-21 10:45:51 +0530

    Add files via upload

---
+++ Project/BankAccount/App1/App1/BankAccountApp1.cs
@@ -0,0 +1,141 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01
+{
+    public class BankAccount
+    {
+        int accountNumber;
+        string name;
+        string password;
+        double balance;
+        double interestRate;
+
+        public BankAccount(int accountNumber, string name, string password, double amount, double interestRate)
+        {
+            this.accountNumber = accountNumber;
+            this.name = name;
+            this.password = password;
+            //SetPassword(password);
+            balance = amount;
+            this.interestRate = interestRate;
+
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine("Amount Deposited");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount");
+            }
+
+        }
+
+        public string Withdraw(double amount, string password)
+        {
+            if (amount < 0)
+            {
+                string output = "Invalid Amount";
+                return output;
+            }
+            else if (amount > balance)
+            {
+                string output = "Insufficient Balance";
+                return output;
+            }
+            else if (this.password != password)
+            {
+                string output = "Invalid Credentials";
+                return output;
+            }
+            else
+            {
+                balance -= amount;
+                string output = "Success";
+                return output;
+            }
+        }
+
+        public void CreditInterest()
+        {
+            balance += balance * interestRate / 1200;
+            Console.WriteLine("Inerest Credited");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Account Number ={0}\tName={1}\tBalance={2}\tInterestRate={3}",
+                accountNumber,
+                name,
+                balance,
+                interestRate); //password not shown
+        }
+
+        public int GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string newName)
+        {
+            if (LastName(name) == LastName(newName))
+            {
+                name = newName;
+            }
+        }
+
+        private string LastName(string name)
+        {
+            int index = name.LastIndexOf(' ');
+            if (index == -1)
+                return "";
+            else
+                return name.Substring(index + 1);
+        }
+
+        public double GetBalance() { return balance; }
+
+        //Can't set the Balance
+        //public void SetBalance(double newBalance) { balance = newBalance; }
+
+
+        public double GetInterestRate() { return interestRate; }
+        public void SetInterestRate(double rate) { interestRate = rate; }
+
+
+        //public string GetPassword() { return password; }    
+        //private void SetPassword(string password)
+        //{
+        //    Encryptor encryptor = new Encryptor();
+        //    this.password = encryptor.Encrypt(password);
+        //}
+
+        //public bool Authenticate(string password)
+        //{
+        //    Encryptor matcher = new Encryptor();
+
+        //    return matcher.Match(this.password, password);
+        //}
+
+        //public void ChangePassword(string confirmPassword, string newPassword)
+        //{
+        //    if (Authenticate(confirmPassword))
+        //        SetPassword(newPassword);
+        //}
+
+    }
+}
---
+++ Project/BankAccount/App1/App1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "pass#1";
+            BankAccount a1 = new BankAccount(1, "Sanjay Mall", password, 20000, 12);
+
+            a1.Show();
+
+            a1.Deposit(2000);
+
+            a1.Deposit(-2);
+
+            a1.Withdraw(1000, password);
+
+            a1.Withdraw(-1, password);
+
+            a1.Withdraw(1000, "wrong password");
+
+            a1.CreditInterest();
+
+            a1.Show();
+
+            a1.SetName("Sanjay Singh"); //shouldn't change
+            Console.WriteLine(a1.GetName());
+
+            a1.SetName("Sanjeev Mall");
+            Console.WriteLine(a1.GetName());
+
+
+
+
+        }
+    }
+}
